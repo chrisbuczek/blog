bb62397b6aa68e5bce6f01705024c332d921e8e1
chrisbuczek <krzysztofbuczek@live.com>
2025-07-13 21:02:42 +0200

    npx create-next-app@latest

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,4 @@
-import nextra from "nextra";
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
 
-// Set up Nextra with its configuration
-const withNextra = nextra({
-  // ... Add Nextra-specific options here
-});
-
-// Export the final Next.js config with Nextra included
-export default withNextra({
-  // ... Add regular Next.js options here
-});
+export default nextConfig;
